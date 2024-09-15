2704d1d1ff2dba7b1a6646a7961f6aab0a5ede28
Udaya-Krishna <118593303+Udaya-Krishna@users.noreply.github.com>
2024-09-15 15:58:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function game1(){
+    window.location.href = "exp.html";
+}
+
+function game2(){
+    window.location.href = "snap.html";
+}
+
+function game3(){
+    window.location.href = "gamification/game1/index.html";
+    
+}
+
+function game4(){
+    window.location.href = "gamification/game2/game3/index.html";
+    
+}
+
+function game5(){
+    window.location.href = "gamification/geometry/index.html";
+}
+
+
+function game8(){
+    window.location.href = "web_maze/index.html"
+}
